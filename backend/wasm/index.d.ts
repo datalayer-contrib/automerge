e7c933c474cf34bd845b4c8f5de3ae8da8b38968
Eric Charles <eric@datalayer.io>
2021-01-01 12:16:00 +0100

    new wasm

--- backend/wasm/index.d.ts
+++ backend/wasm/index.d.ts
@@ -1,54 +1,56 @@
 /* tslint:disable */
 /* eslint-disable */
+/**
+*/
 export class State {
   free(): void;
 /**
-* @param {Array<any>} changes 
-* @returns {any} 
+* @param {Array<any>} changes
+* @returns {any}
 */
   applyChanges(changes: Array<any>): any;
 /**
-* @param {Array<any>} changes 
+* @param {Array<any>} changes
 */
   loadChanges(changes: Array<any>): void;
 /**
-* @param {any} change 
-* @returns {Array<any>} 
+* @param {any} change
+* @returns {Array<any>}
 */
   applyLocalChange(change: any): Array<any>;
 /**
-* @returns {any} 
+* @returns {any}
 */
   getPatch(): any;
 /**
-* @param {any} have_deps 
-* @returns {Array<any>} 
+* @param {any} have_deps
+* @returns {Array<any>}
 */
   getChanges(have_deps: any): Array<any>;
 /**
-* @param {any} actorid 
-* @returns {Array<any>} 
+* @param {any} actorid
+* @returns {Array<any>}
 */
   getChangesForActor(actorid: any): Array<any>;
 /**
-* @returns {any} 
+* @returns {any}
 */
   getMissingDeps(): any;
 /**
-* @returns {State} 
+* @returns {State}
 */
   clone(): State;
 /**
-* @returns {any} 
+* @returns {any}
 */
   save(): any;
 /**
-* @param {any} data 
-* @returns {State} 
+* @param {any} data
+* @returns {State}
 */
   static load(data: any): State;
 /**
-* @returns {State} 
+* @returns {State}
 */
   static new(): State;
 }
